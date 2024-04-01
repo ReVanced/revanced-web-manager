c7215ae3f1693640363c0b91e887f955fdff7db7
Aunali321 <aunvakil.aa@gmail.com>
2024-04-01 22:02:07 +0530

    feat: Setup project structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1>ReVanced Web Manager</h1>
+<p>
+	ReVanced was born out of Vanced's discontinuation and it is our goal to continue the legacy of
+	what Vanced left behind. Thanks to ReVanced Patcher, it's possible to create long-lasting patches
+	for nearly any Android app. ReVanced's patching system is designed to allow patches to work on new
+	versions of the apps automatically with bare minimum maintenance. ReVanced offers a variety of
+	patches, including ad-blocking, custom themes, and innovative features. All of which is completely
+	open source and free of charge.
+</p>
+<p>
+	Visit <a href="https://github.com/revanced/revanced-documentation">ReVanced documentation</a> to read
+	the documentation
+</p>
